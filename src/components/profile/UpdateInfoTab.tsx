34829179a87b393ab9708d092e78ad73fbe5895a
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-09-04 12:34:20 -0600

    Actualizo los valores en el store y en el localstorage. Agrego un botón para habilitar el input y poder editar. Limpio los alerts correctamente.

--- src/components/profile/UpdateInfoTab.tsx
+++ src/components/profile/UpdateInfoTab.tsx
@@ -1,31 +1,56 @@
 import { useState } from "react";
-import { Alert, Button, Form } from "react-bootstrap";
+import { Alert, Button, Form, InputGroup } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { updateUserInfo } from "../../api/users";
-import { useAppSelector } from "../../hooks/useStore";
+import { useAppDispatch, useAppSelector } from "../../hooks/useStore";
 import { RootState } from "../../store";
 import { setUser } from "../../store/features/auth/authSlice";
+import { USER_DATA } from "../../config/environment";
 
 const UpdateInfoTab: React.FC = () => {
 	const user = useAppSelector((state: RootState) => state.auth.user);
+	const dispatch = useAppDispatch();
 	
 	const [username, setUsername] = useState<string | undefined>(user?.nombre);
 	const [email, setEmail] = useState<string | undefined>(user?.email);
 
+	const [isUsernameDisabled, setIsUsernameDisabled] = useState<boolean>(true);
+	const [isEmailDisabled, setIsEmailDisabled] = useState<boolean>(true);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [success, setSuccess] = useState<string>('');
 	const [error, setError] = useState<string>('');
 
+	const resetAlertMessages = () => {
+		setSuccess('');
+		setError('');
+	};
+
+	const resetDisabledValues = () => {
+		setIsUsernameDisabled(true);
+		setIsEmailDisabled(true);
+	};
+
 	const handleSubmit = async (e: React.FormEvent) => {
 		e.preventDefault();
+		resetAlertMessages();
+
 		try {
 			setIsLoading(true);
 			if (!username || !email) {
 				setError('Los campos nombre de usuario y el email son requeridos');
 				return;
 			}
+
 			await updateUserInfo({ name: username, email: email });
-			if (user) setUser({ ...user, nombre: username, email: email });
+			if (user) {
+				const newUser = { ...user, nombre: username, email: email };
+				dispatch(setUser(newUser));
+				localStorage.setItem(USER_DATA, JSON.stringify(newUser));
+			}
+
 			setSuccess('La información se actualizó correctamente');
+			resetDisabledValues();
 		} catch (error) {
 			setError((error as Error).message);
 		} finally {
@@ -40,23 +65,43 @@ const UpdateInfoTab: React.FC = () => {
 			<Form onSubmit={handleSubmit} className="form">
 				<Form.Group className="mb-3">
 					<Form.Label htmlFor="username">Nombre de usuario</Form.Label>
-					<Form.Control
-						type="text"
-						id="username"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-					/>
+					<InputGroup>
+						<Form.Control
+							type="text"
+							id="username"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							required
+							disabled={isUsernameDisabled}
+						/>
+						<Button
+							variant="outline-secondary"
+							onClick={() => setIsUsernameDisabled(!isUsernameDisabled)}
+							className='eye'
+						>
+							<FontAwesomeIcon icon={isUsernameDisabled ? faPenToSquare : faXmark} />
+						</Button>
+					</InputGroup>
 				</Form.Group>
 				<Form.Group className="mb-3">
 					<Form.Label htmlFor="email">Email</Form.Label>
-					<Form.Control
-						type="email"
-						id="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
+					<InputGroup>
+						<Form.Control
+							type="email"
+							id="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							required
+							disabled={isEmailDisabled}
+						/>
+						<Button
+							variant="outline-secondary"
+							onClick={() => setIsEmailDisabled(!isEmailDisabled)}
+							className='eye'
+						>
+							<FontAwesomeIcon icon={isEmailDisabled ? faPenToSquare : faXmark} />
+						</Button>
+					</InputGroup>
 				</Form.Group>
 				<Button variant="primary" type="submit" disabled={isLoading}>
 					{isLoading ? 'Procesando...' : 'Actualizar datos'}
