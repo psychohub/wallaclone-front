d7f5dc410267eb647c6c588b845ee4b3c2f69500
Gabriel Chávez Riera <gabrielchavezriera@gmail.com>
2024-09-12 22:28:59 -0600

    Agrego en axiosInstance el token desde el store si está presente.

--- src/store/features/auth/authSlice.ts
+++ src/store/features/auth/authSlice.ts
@@ -103,8 +103,6 @@ const authSlice = createSlice({
         state.isLoading = false;
         state.user = action.payload.data.usuario;
         state.token = action.payload.data.token;
-        localStorage.setItem(ACCESS_TOKEN, action.payload.data.token);
-        localStorage.setItem(USER_DATA, JSON.stringify(action.payload.data.usuario));
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
